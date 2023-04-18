b4f71e45b0c0ae5af04acce4a8b3e9cfefa304a0
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2023-04-18 14:06:13 -0400

    Transform some files to javascript.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,4 @@
 import '../../styles/index.css';
-// import { Inter,  } from 'next/font/google';
 import AuthProvider from './auth-provider';
 
 export default function Layout({ children }) {
@@ -7,7 +6,6 @@ export default function Layout({ children }) {
     <html lang="en">
       <body>
         <AuthProvider>{children}</AuthProvider>
-        {/* {children} */}
       </body>
     </html>
   )
