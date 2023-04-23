38efff65ae3a5412ca577ed5c59b770cae0a8d8a
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2023-04-23 12:49:52 -0400

    Add mail address to navbar links.

--- src/components/navbar/links/links.jsx
+++ src/components/navbar/links/links.jsx
@@ -7,6 +7,9 @@ export function Links() {
       <li className={classes.listItem}>
         <Link variant="simple" href="/projects">Projects</Link>
       </li>
+      <li className={classes.listItem}>
+        <div className={classes.mailAddress}>contact@sinabyr.ir</div>
+      </li>
     </>
   )
 }
