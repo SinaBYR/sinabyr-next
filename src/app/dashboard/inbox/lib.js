db2268cb2ede204434439a0796306ed028c78eef
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2023-04-23 13:47:18 -0400

    Remove api route for dashboard messages, and fetch directly on server-side.

---
+++ src/app/dashboard/inbox/lib.js
@@ -0,0 +1,11 @@
+import { pool } from "@/lib/database";
+
+export async function getDashboardMessages() {
+  let result = await pool.query(`
+    SELECT id, subject, sent_at
+    FROM message
+    ORDER BY sent_at
+  `);
+
+  return result.rows;
+}
