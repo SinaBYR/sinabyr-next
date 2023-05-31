1165a39b6a110f52ec637c0629a4463b7d4e1c4d
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2023-05-31 07:06:08 -0400

    Remove description from reducedProject, and replace it with title of the project.

--- src/components/showcase/reducedProject/reducedProject.jsx
+++ src/components/showcase/reducedProject/reducedProject.jsx
@@ -10,13 +10,12 @@ export function ReducedProject({ id, title, description, thumbnail, techList }) 
           src={thumbnail || 'https://sinabyr.storage.iran.liara.space/skills-icons/project_preview_fallback.png'}
           alt="project-preview-screenshot"
           layout="fill"/>
-        <div className={classes.title}>{title}</div>
       </div>
       <div className={classes.content}>
         <div className={classes.tech}>
           {techList.map(val => <span key={val}>{val}</span>)}
         </div>
-        <p dangerouslySetInnerHTML={{__html: description}} />
+        <h3 className={classes.title}>{title}</h3>
         <Link variant="secondary" href={"/projects/" + id}>Read more</Link>
       </div>
     </div>
